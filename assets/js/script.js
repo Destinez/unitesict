d3491cab5cbf565867a3bb566e0511469af4395b
Obaro Destiny Udurie <obarodestinez@gmail.com>
2022-06-20 11:10:48 +0100

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(".owl-carousel").owlCarousel({
+  items: 1,
+  loop: true,
+  margin: 0,
+  nav: true,
+  margin: 0,
+  autoplay: true,
+  autoplayTimeout: 15000,
+  autoplayHoverPause: true,
+});
